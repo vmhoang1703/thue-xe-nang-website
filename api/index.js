1a5476e7adccb304ee97c075f888594037af20e1
vmhoang1703 <vmhoang1703@gmail.com>
2024-04-13 14:43:56 +0700

    update

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const newsRoutes = require('./routes/news.routes');
 const dbConnect = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
